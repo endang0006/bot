3395d1027675e72467e1c642897c5011d00659a2
endang0006 <64590296+endang0006@users.noreply.github.com>
2020-05-10 21:37:42 +0700

    Add files via upload

---
+++ bot2.sh
@@ -0,0 +1,10 @@
+#Note :
+#isinomor ganti dengan nomor kalian
+#formatcoin ganti dengan format bot yang kalian inginkan
+#contoh format coin ( btc, bch,ltc,zec,doge )
+python bot.py +18322461818 bch &&
+python bot.py +16234730968 bch &&
+python bot.py +16233836565 bch &&
+python bot.py +16285005976 bch &&
+python bot.py +16234018082 bch &&
+sh bot2.sh
