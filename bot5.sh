ac9a5d9113e182c7324cc299ec9bd759b0831269
endang0006 <64590296+endang0006@users.noreply.github.com>
2020-05-10 21:44:30 +0700

    Update bot5.sh

--- bot5.sh
+++ bot5.sh
@@ -3,11 +3,7 @@
 #formatcoin ganti dengan format bot yang kalian inginkan
 #contoh format coin ( btc, bch,ltc,zec,doge )
 python bot.py +16292223878 bch &&
-sleep 0
 python bot.py +16234733388 bch &&
-sleep 0
 python bot.py +16232096222 bch &&
-sleep 0
 python bot.py +16292225765 bch &&
-sleep 0
 sh bot5.sh
